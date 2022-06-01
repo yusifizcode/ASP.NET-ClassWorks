d0509956d7dca662da2be58abb82c339937ccc28
yusifizcode <5ba0gh2@code.edu.az>
2022-06-01 13:11:03 +0400

    Eterna site Portfolio Section

--- ASP.NET-Model-ViewModel-Layout/EternaSite/Controllers/PortFolioController.cs
+++ ASP.NET-Model-ViewModel-Layout/EternaSite/Controllers/PortFolioController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EternaSite.DAL;
+using EternaSite.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace EternaSite.Controllers
 {
     public class PortFolioController : Controller
     {
+        private AppDbContext _context;
+
+        public PortFolioController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Portfolio()
         {
-            return View();
+            PortfolioViewModel portfolioVM = new PortfolioViewModel
+            {
+                Portfolios = _context.Portfolios.Include(x => x.Category).ToList(),
+                Categories = _context.Categories.ToList()
+            };
+
+            return View(portfolioVM);
         }
     }
 }
---
+++ ASP.NET-Model-ViewModel-Layout/EternaSite/Migrations/20220601085733_PortfoliosTableCreated.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EternaSite.Migrations
+{
+    public partial class PortfoliosTableCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
